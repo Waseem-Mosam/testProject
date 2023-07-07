898b45f5513b0db92acb1044223cb293e5fd5904
Waseem-Mosam <waseem.mosam1@gmail.com>
2023-07-07 09:02:23 +0200

    Update settings.py

--- testProject/settings.py
+++ testProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q6hhuha&bgzs$#oyotzoyj**^bda%a$le-3n4k=g*v%=)uhw=+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
